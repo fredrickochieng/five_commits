a8a4926c61c2a1d12553753255b56148d42a927d
fredrickochieng <fredrickochieng2013@gmail.com>
2017-01-17 03:07:44 +0300

    This is shows a sample way of adding new entries into a dictionary

---
+++ dict.py
@@ -0,0 +1,11 @@
+menu = {} 
+menu['Chicken'] =50.20
+print menu['Chicken']
+
+menu['Sukuma'] = 10.50
+menu['omena'] = 18.50
+menu['Fish A'] = 11.50
+menu['Lamb rice'] = 20.40 # Your code here: Add some dish-price pairs to menu!
+
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
