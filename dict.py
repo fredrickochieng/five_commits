e1446d75caf2bdedc1aa34cc33505e488ea0a082
fredrickochieng <fredrickochieng2013@gmail.com>
2017-01-17 03:10:54 +0300

    i just changed the menu pricess,check it out

--- dict.py
+++ dict.py
@@ -1,11 +1,11 @@
 menu = {} 
-menu['Chicken'] =50.20
+menu['Chicken'] =100
 print menu['Chicken']
 
-menu['Sukuma'] = 10.50
-menu['omena'] = 18.50
-menu['Fish A'] = 11.50
-menu['Lamb rice'] = 20.40 # Your code here: Add some dish-price pairs to menu!
+menu['Sukuma'] = 60
+menu['omena'] = 110
+menu['Fish A'] = 300
+menu['Lamb rice'] = 150 # Your code here: Add some dish-price pairs to menu!
 
 print "There are " + str(len(menu)) + " items on the menu."
 print menu
